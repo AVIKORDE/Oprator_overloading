08f895cb824a6044cc04fbb86f0e39c70dea55d8
Avinash Santosh Korde <kordeavinash86@gmail.com>
2024-05-21 09:30:41 +0530

    Add project files.

---
+++ Oprator_overloading/Oprators_overload.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oprator_overloading
+{
+    
+    class Employee
+    {
+
+        int id;
+        string name;
+        int sal;
+        string dep;
+        public int getid()
+        { return id; }
+        public void setid(int id)
+        { this.id = id; }
+
+        static public Employee operator +(Employee emp1, Employee emp2)
+        {
+            Employee employee = new Employee(); 
+            employee.sal = emp1.sal+emp2.sal;
+            return employee;
+        }
+
+        static public bool operator true(Employee emp)
+        {
+            if (emp.sal<25000)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        static public bool operator false(Employee emp)
+        {
+            if (emp.sal<250000)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        static public bool operator == (Employee emp1, Employee emp2)
+        {
+            if (emp1.sal == emp2.sal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static public bool operator !=(Employee emp1, Employee emp2)
+        {
+            if (emp1.sal != emp2.sal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Employee()
+        {
+
+        }
+        public Employee(int i, string nm, int sa,string dept)
+        {
+            id = i;
+            name = nm;
+            sal = sa;
+            dep = dept;
+        }
+        public void showEmployee()
+        {
+            Console.WriteLine(" id= " + id + " Name=" + name + " salary=" + sal+"department ="+dep);
+        }
+       
+
+    }
+    internal class Oprators_overload
+    {
+    }
+}
---
+++ Oprator_overloading/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Oprator_overloading;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Employee e1= new Employee(1,"avi",10000,"computer");
+        Employee e2 = new Employee(2, "sarthak", 26000, "it");
+        e1.showEmployee();
+        e2.showEmployee();
+        if (e1 == e2)
+        {
+            Console.WriteLine("Both having same sal");
+        }
+        else
+        {
+            Console.WriteLine("different sal...");
+        }
+        if(e1)
+        {
+            Console.WriteLine("salary is greater than 25000");
+        }
+        else {
+            Console.WriteLine("salary is less than 25000");
+        }
+        if (e2)
+        {
+            Console.WriteLine("salary is greater than 25000");
+        }
+        else
+        {
+            Console.WriteLine("salary is less than 25000");
+        }
+
+        Employee e3 = e1 + e2;
+        Console.WriteLine(e3);
+
+
+    }
+}

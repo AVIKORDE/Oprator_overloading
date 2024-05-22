bdfb3cf57b649705194f4ba8e1910ed284330a5d
Avinash Santosh Korde <kordeavinash86@gmail.com>
2024-05-22 08:54:00 +0530

    changes

---
+++ Oprator_overloading/Oper_overloading2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oprator_overloading
+{
+    class Circle
+
+    {
+
+        int radius;
+        static public bool operator ==(Circle c1, Circle c2)
+        {
+            if (c1.radius == c2.radius)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static public bool operator !=(Circle c1, Circle c2)
+        {
+            if (c1.radius != c2.radius)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Circle()
+            {
+        }
+
+        public  Circle(int rad)
+        {
+            radius = rad;
+        }
+        static public Circle operator ++(Circle c1)
+        {
+            Console.WriteLine("++ operator executed!!!!");
+            c1.radius++;
+            return c1;
+        }
+        public void Area()
+        {
+           int area = radius * radius;
+            Console.WriteLine("Area of circle is="+area);
+        }
+        
+    }
+
+    internal class Oper_overloading2
+    {
+    }
+}
--- Oprator_overloading/Program.cs
+++ Oprator_overloading/Program.cs
@@ -6,37 +6,55 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Employee e1= new Employee(1,"avi",10000,"computer");
-        Employee e2 = new Employee(2, "sarthak", 26000, "it");
-        e1.showEmployee();
-        e2.showEmployee();
-        if (e1 == e2)
+        #region
+
+        //Employee e1= new Employee(1,"avi",10000,"computer");
+        //Employee e2 = new Employee(2, "sarthak", 26000, "it");
+        //e1.showEmployee();
+        //e2.showEmployee();
+        //if (e1 == e2)
+        //{
+        //    Console.WriteLine("Both having same sal");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("different sal...");
+        //}
+        //if(e1)
+        //{
+        //    Console.WriteLine("salary is greater than 25000");
+        //}
+        //else {
+        //    Console.WriteLine("salary is less than 25000");
+        //}
+        //if (e2)
+        //{
+        //    Console.WriteLine("salary is greater than 25000");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("salary is less than 25000");
+        //}
+
+        //Employee e3 = e1 + e2;
+        //Console.WriteLine(e3);
+        #endregion
+        Circle circ1 = new Circle(10);
+        Circle circ2 = new Circle(20);
+        if(circ1 == circ2)
         {
-            Console.WriteLine("Both having same sal");
+            Console.WriteLine("same circle");
+
         }
         else
         {
-            Console.WriteLine("different sal...");
+            Console.WriteLine("Not same");
         }
-        if(e1)
-        {
-            Console.WriteLine("salary is greater than 25000");
-        }
-        else {
-            Console.WriteLine("salary is less than 25000");
-        }
-        if (e2)
-        {
-            Console.WriteLine("salary is greater than 25000");
-        }
-        else
-        {
-            Console.WriteLine("salary is less than 25000");
-        }
-
-        Employee e3 = e1 + e2;
-        Console.WriteLine(e3);
-
+        circ1++;
+        circ2++;
+        circ1.Area();
+        circ2.Area();
+       
 
     }
 }
